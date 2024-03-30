dcd27d4157502af964461fa2501ae946435926fe
varunmsaji <varunmsaji01@gmail.com>
2024-03-30 21:40:48 +0530

    initial commit with base code

---
+++ nlp.py
@@ -0,0 +1,9 @@
+#remove the stopwords and stem the content
+ps = PorterStemmer()
+def stemming(content):
+    stemmed_content = re.sub('[^a-zA-Z]',' ',content)
+    stemmed_content = stemmed_content.lower()
+    stemmed_content = stemmed_content.split()
+    stemmed_content = [ps.stem(word) for word in stemmed_content if not word in stopwords.words('english')]
+    stemmed_content = ' '.join(stemmed_content)
+    return stemmed_content
